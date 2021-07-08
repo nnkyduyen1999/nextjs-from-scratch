44d0481af08d5df4ff1fcbf6067d01cbe4390d83
1712397 <nnkduyen1999@gmail.com>
2021-07-08 17:11:46 +0700

    reusable components

---
+++ components/Navbar/navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div>
+      <div className="logo">Duyen</div>
+      <nav>
+        <a>Home</a>
+        <a>About</a>
+        <a>Girls</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import Footer from "../components/Footer/footer";
+import Navbar from "../components/Navbar/navbar";
+
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Home</h1>
+      <p>This is home pageeeeeee..........</p>
+      <Footer />
     </div>
-  )
+  );
 }
