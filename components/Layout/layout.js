1540af6c8a30ba70f1bd8ace2edda350e61f1037
1712397 <nnkduyen1999@gmail.com>
2021-07-08 17:56:11 +0700

    layout for page in app.js

---
+++ components/Layout/layout.js
@@ -0,0 +1,14 @@
+import Navbar from "../Navbar/navbar";
+import Footer from "../Footer/footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import Footer from "../components/Footer/footer";
-import Navbar from "../components/Navbar/navbar";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Home</h1>
       <p>This is home pageeeeeee..........</p>
-      <Footer />
     </div>
   );
 }
