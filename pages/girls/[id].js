5665b878b52c138cc5f0a883feb283eb30c69fa8
1712397 <nnkduyen1999@gmail.com>
2021-07-10 17:13:51 +0700

    render data for dynamic page

--- pages/girls/[id].js
+++ pages/girls/[id].js
@@ -1,13 +1,49 @@
 import Link from "next/link";
 import homeStyles from "../../styles/Home.module.css";
 
-const CatDetail = () => {
+export const getStaticPaths = async () => {
+  const listCats = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const catsAsJson = await listCats.json();
+
+  // this return the param each item need to receive
+  // in this case param for each cat is the id, this may have more inside the params props
+  const pathListByItem = catsAsJson.map((cat) => ({
+    params: {
+      id: cat.id.toString(),
+    },
+  }));
+
+  return {
+    paths: pathListByItem, //this is the list of params obj that contain the param, each item in this arr will be save in the context to use in getStaticProps func
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const idCat = context.params.id;
+  console.log(context);
+  const catDetail = await fetch(
+    `https://jsonplaceholder.typicode.com/users/` + idCat
+  );
+  const catAsJson = await catDetail.json();
+  return {
+    props: {
+      cat: catAsJson,
+      id: idCat,
+    },
+  };
+};
+
+const CatDetail = ({cat}) => {
   return (
     <div>
-      <h1>Cat Detail</h1>
+      <h1>Cat {cat.name}</h1>
+      <p>Email: {cat.email}</p>
+      <p>City: {cat.address.city}</p>
+      <p>Website: {cat.website}</p>
       <Link href="/girls">
-          <a className={homeStyles.btn}>To Girls</a>
-        </Link>
+        <a className={homeStyles.btn}>To Girls</a>
+      </Link>
     </div>
   );
 };
