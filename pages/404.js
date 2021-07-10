2e2e2f5398b14559510a2dc9db2e4251127c8cbd
1712397 <nnkduyen1999@gmail.com>
2021-07-10 15:08:55 +0700

    redirect and metadata

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,26 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const NotFoundPage = () => {
+  const router = useRouter();
+  const [countdowntime, setCountdowntime] = useState(5);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCountdowntime((time) => time - 1);
+    }, 1000);
+    if (countdowntime <= 0) {
+      router.push("/");
+    }
+    return () => clearInterval(id);
+  }, [countdowntime]);
+
   return (
     <div className="notFound">
       <h1>Opppppps...</h1>
       <h2>That page cannot be found.</h2>
+      <h3>Return to Home in {countdowntime}s.</h3>
       <p>
         Go back to
         <Link href="/">
